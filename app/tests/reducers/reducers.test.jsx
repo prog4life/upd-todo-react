383672d77bd148f071b03bb626ddc51320973bad
prog4life <prog4life@users.noreply.github.com>
2017-07-08 21:13:21 +0300

    Add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -26,5 +26,38 @@ describe('Reducers', () => {
       expect(res).toEqual(true);
     });
   });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Walk in city center'
+      };
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle todo', () => {
+      var todos = [
+        {
+          id: 155,
+          text: 'Something',
+          completed: true,
+          createdAt: 145897,
+          completedAt: 145899
+        }
+      ];
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: 155
+      };
+      var res = reducers.todosReducer(df(todos), df(action));
+
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toEqual(undefined);
+    });
+  });
 });
 
