9d297741f26dd6b102afac47be041f4c867591b9
prog4life <prog4life@users.noreply.github.com>
2017-07-09 20:36:59 +0300

    Refactor TodoList, Todo and AddTodo to work with redux

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -42,8 +42,9 @@ export var todosReducer = (state = [], action) => {
             completed: nextCompleted,
             completedAt: nextCompleted ? moment().unix() : undefined
           };
+        } else {
+          return todo;
         }
-        return todo;
       });
     default:
       return state;
