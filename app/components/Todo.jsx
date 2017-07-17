8eadc44a5493153799083af2f9b896025f724984
prog4life <prog4life@users.noreply.github.com>
2017-07-17 23:56:38 +0300

    Process Todo and TodoList components

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,10 +1,10 @@
-var React = require('react');
-var {connect} = require('react-redux');
-var moment = require('moment');
-var actions = require('actions');
+import React from 'react';
+import {connect} from 'react-redux';
+import moment from 'moment';
+import actions from 'actions';
 
-export var Todo = React.createClass({
-  render: function () {
+export class Todo extends React.Component {
+  render () {
     var {id, text, completed, createdAt, completedAt, dispatch} = this.props;
     var todoClassName = completed ? 'todo todo-completed' : 'todo';
     var renderDate = () => {
@@ -33,6 +33,6 @@ export var Todo = React.createClass({
       </div>
     );
   }
-});
+}
 
 export default connect()(Todo);
--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -1,10 +1,10 @@
-var React = require('react');
-var {connect} = require('react-redux');
+import React from 'react';
+import {connect} from 'react-redux';
 import Todo from 'Todo';
-var TodoAPI = require('TodoAPI');
+import TodoAPI from 'TodoAPI';
 
-export var TodoList = React.createClass({
-  render: function () {
+export class TodoList extends React.Component {
+  render () {
     var {todos, showCompleted, searchText} = this.props;
 
     var renderTodos = () => {
@@ -29,7 +29,7 @@ export var TodoList = React.createClass({
       </div>
     );
   }
-});
+}
 
 export default connect(
   (state) => {
